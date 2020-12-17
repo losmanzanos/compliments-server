26de58a8937d7b9c7229a1631044b665658e657a
Chad Moravec <chad.moravec@colorado.edu>
2020-12-17 13:46:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const path = require("path");
-
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Malcom in the Middleware...
